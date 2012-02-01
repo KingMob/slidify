7a8783e8a5016d5abe0ff30055afcec280b4a1d5
Matthew Davidson <sunyata@gmail.com>
2012-02-01 01:06:03 -0500

    Switched to supersized. Got basic slideshow working.

--- 4chan-slideshow.user.js
+++ 4chan-slideshow.user.js
@@ -9,6 +9,9 @@
 
 // the guts of this userscript
 function main() {
+	jQuery.easing.jswing=jQuery.easing.swing;jQuery.extend(jQuery.easing,{def:"easeOutQuad",swing:function(e,f,a,h,g){return jQuery.easing[jQuery.easing.def](e,f,a,h,g)},easeInQuad:function(e,f,a,h,g){return h*(f/=g)*f+a},easeOutQuad:function(e,f,a,h,g){return -h*(f/=g)*(f-2)+a},easeInOutQuad:function(e,f,a,h,g){if((f/=g/2)<1){return h/2*f*f+a}return -h/2*((--f)*(f-2)-1)+a},easeInCubic:function(e,f,a,h,g){return h*(f/=g)*f*f+a},easeOutCubic:function(e,f,a,h,g){return h*((f=f/g-1)*f*f+1)+a},easeInOutCubic:function(e,f,a,h,g){if((f/=g/2)<1){return h/2*f*f*f+a}return h/2*((f-=2)*f*f+2)+a},easeInQuart:function(e,f,a,h,g){return h*(f/=g)*f*f*f+a},easeOutQuart:function(e,f,a,h,g){return -h*((f=f/g-1)*f*f*f-1)+a},easeInOutQuart:function(e,f,a,h,g){if((f/=g/2)<1){return h/2*f*f*f*f+a}return -h/2*((f-=2)*f*f*f-2)+a},easeInQuint:function(e,f,a,h,g){return h*(f/=g)*f*f*f*f+a},easeOutQuint:function(e,f,a,h,g){return h*((f=f/g-1)*f*f*f*f+1)+a},easeInOutQuint:function(e,f,a,h,g){if((f/=g/2)<1){return h/2*f*f*f*f*f+a}return h/2*((f-=2)*f*f*f*f+2)+a},easeInSine:function(e,f,a,h,g){return -h*Math.cos(f/g*(Math.PI/2))+h+a},easeOutSine:function(e,f,a,h,g){return h*Math.sin(f/g*(Math.PI/2))+a},easeInOutSine:function(e,f,a,h,g){return -h/2*(Math.cos(Math.PI*f/g)-1)+a},easeInExpo:function(e,f,a,h,g){return(f==0)?a:h*Math.pow(2,10*(f/g-1))+a},easeOutExpo:function(e,f,a,h,g){return(f==g)?a+h:h*(-Math.pow(2,-10*f/g)+1)+a},easeInOutExpo:function(e,f,a,h,g){if(f==0){return a}if(f==g){return a+h}if((f/=g/2)<1){return h/2*Math.pow(2,10*(f-1))+a}return h/2*(-Math.pow(2,-10*--f)+2)+a},easeInCirc:function(e,f,a,h,g){return -h*(Math.sqrt(1-(f/=g)*f)-1)+a},easeOutCirc:function(e,f,a,h,g){return h*Math.sqrt(1-(f=f/g-1)*f)+a},easeInOutCirc:function(e,f,a,h,g){if((f/=g/2)<1){return -h/2*(Math.sqrt(1-f*f)-1)+a}return h/2*(Math.sqrt(1-(f-=2)*f)+1)+a},easeInElastic:function(f,h,e,l,k){var i=1.70158;var j=0;var g=l;if(h==0){return e}if((h/=k)==1){return e+l}if(!j){j=k*0.3}if(g<Math.abs(l)){g=l;var i=j/4}else{var i=j/(2*Math.PI)*Math.asin(l/g)}return -(g*Math.pow(2,10*(h-=1))*Math.sin((h*k-i)*(2*Math.PI)/j))+e},easeOutElastic:function(f,h,e,l,k){var i=1.70158;var j=0;var g=l;if(h==0){return e}if((h/=k)==1){return e+l}if(!j){j=k*0.3}if(g<Math.abs(l)){g=l;var i=j/4}else{var i=j/(2*Math.PI)*Math.asin(l/g)}return g*Math.pow(2,-10*h)*Math.sin((h*k-i)*(2*Math.PI)/j)+l+e},easeInOutElastic:function(f,h,e,l,k){var i=1.70158;var j=0;var g=l;if(h==0){return e}if((h/=k/2)==2){return e+l}if(!j){j=k*(0.3*1.5)}if(g<Math.abs(l)){g=l;var i=j/4}else{var i=j/(2*Math.PI)*Math.asin(l/g)}if(h<1){return -0.5*(g*Math.pow(2,10*(h-=1))*Math.sin((h*k-i)*(2*Math.PI)/j))+e}return g*Math.pow(2,-10*(h-=1))*Math.sin((h*k-i)*(2*Math.PI)/j)*0.5+l+e},easeInBack:function(e,f,a,i,h,g){if(g==undefined){g=1.70158}return i*(f/=h)*f*((g+1)*f-g)+a},easeOutBack:function(e,f,a,i,h,g){if(g==undefined){g=1.70158}return i*((f=f/h-1)*f*((g+1)*f+g)+1)+a},easeInOutBack:function(e,f,a,i,h,g){if(g==undefined){g=1.70158}if((f/=h/2)<1){return i/2*(f*f*(((g*=(1.525))+1)*f-g))+a}return i/2*((f-=2)*f*(((g*=(1.525))+1)*f+g)+2)+a},easeInBounce:function(e,f,a,h,g){return h-jQuery.easing.easeOutBounce(e,g-f,0,h,g)+a},easeOutBounce:function(e,f,a,h,g){if((f/=g)<(1/2.75)){return h*(7.5625*f*f)+a}else{if(f<(2/2.75)){return h*(7.5625*(f-=(1.5/2.75))*f+0.75)+a}else{if(f<(2.5/2.75)){return h*(7.5625*(f-=(2.25/2.75))*f+0.9375)+a}else{return h*(7.5625*(f-=(2.625/2.75))*f+0.984375)+a}}}},easeInOutBounce:function(e,f,a,h,g){if(f<g/2){return jQuery.easing.easeInBounce(e,f*2,0,h,g)*0.5+a}return jQuery.easing.easeOutBounce(e,f*2-g,0,h,g)*0.5+h*0.5+a}});
+	
+	
 	/*
 
 		Supersized - Fullscreen Slideshow jQuery Plugin
@@ -1272,26 +1275,54 @@ function main() {
 	// My actual code - Matthew Davidson
 	var imgAnchors = $("form[name=delform] a:parent[href$=jpeg], form[name=delform] a[href$=jpg], form[name=delform] a[href$=png], form[name=delform] a[href$=gif]").has("img");
 	var imgUrls = $.makeArray(imgAnchors.map(function(){return {image: this.href};}));
-	var slider = $("<div id='sliderDiv'><ul id='slider'><li><img src='" + imgUrls.join("'></li><li><img src='") + "'></li></ul></div>");
-	//var slider = $("<div id='sliderDiv'><ul id='slider'><li><img src='" + imgUrls.slice(0, 3).join("'></li><li><img src='") + "'></li></ul></div>");
-	//$("body").append(slider);
-	
-	$('#sliderDiv').css("width", $(window).width());
-	$('#sliderDiv').css("height", $(window).height());
-	//$('#sliderDiv').css("width", $(window).width() - 200);
-	//$('#sliderDiv').css("height", 400);
-	//$('#sliderDiv').css("position", "fixed");
+	var slideshowhtml = $('<!--Thumbnail Navigation-->\
+		<div id="prevthumb"></div>\
+		<div id="nextthumb"></div>\
+		\
+		<!--Arrow Navigation-->\
+		<a id="prevslide" class="load-item"></a>\
+		<a id="nextslide" class="load-item"></a>\
+		\
+		<div id="thumb-tray" class="load-item">\
+			<div id="thumb-back"></div>\
+			<div id="thumb-forward"></div>\
+		</div>\
+		\
+		<!--Control Bar-->\
+		<div id="controls-wrapper" class="load-item">\
+			<div id="controls">\
+			\
+				<a id="play-button"><img id="pauseplay" src="img/pause.png"/></a>\
+				\
+				<!--Slide counter-->\
+				<div id="slidecounter">\
+					<span class="slidenumber"></span> / <span class="totalslides"></span>\
+				</div>\
+			\
+				<!--Slide captions displayed here-->\
+				<div id="slidecaption"></div>\
+			\
+				<!--Thumb Tray button-->\
+				<a id="tray-button"><img id="tray-arrow" src="img/button-tray-up.png"/></a>\
+			\
+				<!--Navigation-->\
+				<ul id="slide-list"></ul>\
+			\
+			</div>\
+		</div>\
+	');
+	$("body").append(slideshowhtml);
 	
 	$.supersized({
 		autoplay				: 	0, 
 		image_protect			: 	0,
 		slide_interval          :   8000,		// Length between transitions
 		transition              :   3, 			// 0-None, 1-Fade, 2-Slide Top, 3-Slide Right, 4-Slide Bottom, 5-Slide Left, 6-Carousel Right, 7-Carousel Left
-		transition_speed		:	700,		// Speed of transition
+		transition_speed		:	400,		// Speed of transition
 												   
 		// Components							
 		slide_links	:	'blank',	// Individual links for each slide (Options: false, 'num', 'name', 'blank')
-		slides 		:  	imgUrls.slice(0, 3)
+		slides 		:  	imgUrls.slice(0, 10)
 	});
 }
 
@@ -1306,6 +1337,7 @@ function main() {
 	Company : One Mighty Roar (www.onemightyroar.com)
 	License : MIT License / GPL License
 */
+
 GM_addStyle("\
 \
 * { margin:0; padding:0; }\
